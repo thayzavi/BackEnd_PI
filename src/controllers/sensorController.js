548c85dae7e5b28af3d8defc337779a398c59e75
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-11-27 17:30:04 +0000

    fix: envio de email

--- src/controllers/sensorController.js
+++ src/controllers/sensorController.js
@@ -3,32 +3,33 @@ import { sendAlertEmail } from "../service/emailService.js";
 import { lastLevel, updateLevel } from "../utils/state.js";
 
 export async function receiveData(req, res) {
-    const { distancia } = req.body;
+    const { distancia, nivel } = req.body;
 
     if (distancia === undefined) {
         return res.status(400).json({ error: "Distância não enviada" });
     }
 
-    let level = "normal";
-    if (distancia <= 5) level = "enchentes";
-    else if (distancia <= 20) level = "medio";
+    // Mapeia nível vindo da ESP32
+    const configNiveis = {
+        0: "normal",
+        1: "medio",
+        2: "enchentes"
+    };
+
+    let level = configNiveis[nivel] || "normal";
 
     const medicao = await Medicao.create({ distancia, level });
 
     if (level !== lastLevel) {
-        try{
+        try {
             updateLevel(level);
             await sendAlertEmail(distancia, level);
             console.log("Email enviado com sucesso!");
-        }   catch (err) {
-            console.error("Error ao enviar email:", err);
+        } catch (err) {
+            console.error("Erro ao enviar email:", err);
         }
     }
 
     return res.status(201).json({ message: "Dado registrado com sucesso", medicao });
 }
 
-export async function getAllData(req, res) {
-    const medicoes = await Medicao.find().sort({ createdAt: -1 });
-    return res.json(medicoes);
-}
